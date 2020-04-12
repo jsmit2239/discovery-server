a9d5ff570aea4889bd8a338f89eac8f426a9f94e
Joe <jsmit2239@gmail.com>
2020-04-11 22:09:38 -0400

    initial commit

---
+++ src/test/java/com/jsmit2239/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsmit2239.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
